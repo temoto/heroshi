7521908d2fee2266e039b631eb8685b82f5f9662
Sergey Shepelev <temotor@gmail.com>
2010-03-04 23:30:24 +0300

    added sleeping_wsgi small utility script which runs a wsgi server that responds with delay

---
+++ tests/sleeping_wsgi.py
@@ -0,0 +1,22 @@
+from eventlet import listen, sleep, spawn, wsgi
+import random
+import webob
+
+
+def server(request):
+    response = webob.Response()
+    pause = 1 + random.random() * 5
+    sleep(pause)
+    response.body = "Slept well for %f sec." % (pause,)
+    return response
+
+def wsgi_app(environ, start_response):
+    request = webob.Request(environ)
+    response = server(request)
+    return response(environ, start_response)
+
+def main():
+    sock = listen( ('127.0.0.1', 9293) )
+    wsgi.server(sock, wsgi_app)
+
+if __name__ == "__main__": main()
