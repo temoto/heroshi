9879005497791d13bb409c1b0a1e8a1ec5af6e36
Sergey Shepelev <temotor@gmail.com>
2010-03-06 09:40:51 +0300

    added contact info into REAL_USER_AGENT

--- shared/__init__.py
+++ shared/__init__.py
@@ -1,6 +1,6 @@
 HEROSHI_NAME = "heroshi"
 HEROSHI_VERSION = '0.1'
+REAL_USER_AGENT = 'HeroshiBot/%s (+http://github.com/temoto/heroshi; temotor@gmail.com)' % HEROSHI_VERSION
 BIND_PORT = 15822
-REAL_USER_AGENT = 'HeroshiBot/%s' % HEROSHI_VERSION
 TIME_FORMAT = "%Y-%m-%dT%H:%M:%S"
 
