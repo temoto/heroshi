e505378e9de2d033595203d14498e584bc8e84ba
Sergey Shepelev <temotor@gmail.com>
2010-03-17 08:14:57 +0300

    manager: cli_manager, separate entry-point which runs eventlet.wsgi.
    Now bin/heroshid understands -v/--verbose and -q/--quiet arguments.
    And you can still run heroshi.manager.server.wsgi_app with your favourite WSGI server.

---
+++ heroshi/manager/cli_manager.py
@@ -0,0 +1,48 @@
+"""Heroshi URL server entry-point. Run this to start serving URLs for workers."""
+
+import eventlet, eventlet.wsgi
+import logging
+from optparse import OptionParser
+
+import heroshi
+from heroshi.conf import settings
+from heroshi.misc import update_loggers_level
+from .server import wsgi_app
+
+
+class Blackhole(object):
+    """File-like object which just discards all writes."""
+
+    def write(self, data):
+        pass
+
+
+def parse_params():
+    usage_info = u"Usage: %prog [OPTION...] [URL...]"
+    version_info = u"Heroshi/%s" % heroshi.__version__
+    opt_parser = OptionParser(usage_info, version=version_info)
+    opt_parser.set_defaults(verbose=False, quiet=False, queue_size=500, connections=500)
+    opt_parser.add_option('-q', '--quiet', action="store_true",
+                          help=u"Be quiet, don't generate any output")
+    opt_parser.add_option('-v', '--verbose', action="store_true",
+                          help=u"Be verbose, print detailed information")
+    options, args = opt_parser.parse_args()
+    return options, args
+
+def main():
+    options, args = parse_params()
+
+    # set up logging
+    if options.quiet:
+        update_loggers_level(logging.CRITICAL)
+    elif options.verbose:
+        update_loggers_level(logging.DEBUG)
+    else:
+        update_loggers_level(settings.log['level'])
+
+    sock = eventlet.listen( ('0.0.0.0', 8080) )
+    eventlet.wsgi.server(sock, wsgi_app, log=Blackhole())
+
+
+if __name__ == '__main__':
+    main()
