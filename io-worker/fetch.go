ff6a466691ca0903eee92de3e3ed83d717a54f47
Sergey Shepelev <temotor@gmail.com>
2010-08-01 02:41:10 +0400

    * New io-worker in Go

---
+++ io-worker/fetch.go
@@ -0,0 +1,148 @@
+package main
+
+import (
+    "encoding/base64"
+    "fmt"
+    "http"
+    "io"
+    "io/ioutil"
+    "net"
+    "os"
+    "strings"
+)
+
+// Copied from httplib.go
+type Client struct {
+    conn    *http.ClientConn
+    LastURL *http.URL
+}
+
+type FetchResult struct {
+    Url        string
+    Success    bool
+    Status     string
+    StatusCode int
+    Body       string
+    Headers    map[string]string
+}
+
+func ErrorResult(url, reason string) *FetchResult {
+    return &FetchResult{
+        Url:     url,
+        Success: false,
+        Status:  reason,
+    }
+}
+
+// Copied from $(GOROOT)/src/pkg/http/client.go
+//
+// Used in Send to implement io.ReadCloser by bundling together the
+// io.BufReader through which we read the response, and the underlying
+// network connection.
+type readClose struct {
+    io.Reader
+    io.Closer
+}
+
+// Copied from $(GOROOT)/src/pkg/http/client.go
+//
+// Given a string of the form "host", "host:port", or "[ipv6::address]:port",
+// return true if the string includes a port.
+func hasPort(s string) bool { return strings.LastIndex(s, ":") > strings.LastIndex(s, "]") }
+
+// Copied from $(GOROOT)/src/pkg/http/client.go
+//
+// True if the specified HTTP status code is one for which the Get utility should
+// automatically redirect.
+func ShouldRedirect(statusCode int) bool {
+    switch statusCode {
+    case http.StatusMovedPermanently, http.StatusFound, http.StatusSeeOther,
+        http.StatusTemporaryRedirect:
+        //
+        return true
+    }
+    return false
+}
+
+// Mostly copy of unexported http.send
+func (client *Client) Request(req *http.Request) (resp *http.Response, err os.Error) {
+    if req.URL.Scheme != "http" {
+        return nil, os.NewError(fmt.Sprint("unsupported protocol scheme", req.URL.Scheme))
+    }
+
+    if client.conn == nil || client.LastURL == nil ||
+        client.LastURL.Host != req.URL.Host {
+        //
+        addr := req.URL.Host
+        if !hasPort(addr) {
+            addr += ":http"
+        }
+
+        creds := req.URL.Userinfo
+        if len(creds) > 0 {
+            enc := base64.URLEncoding
+            encoded := make([]byte, enc.EncodedLen(len(creds)))
+            enc.Encode(encoded, []byte(creds))
+            if req.Header == nil {
+                req.Header = make(map[string]string)
+            }
+            req.Header["Authorization"] = "Basic " + string(encoded)
+        }
+
+        var tcpConn net.Conn
+        if tcpConn, err = net.Dial("tcp", "", addr); err != nil {
+            return nil, err
+        }
+        client.conn = http.NewClientConn(tcpConn, nil)
+    }
+
+    err = client.conn.Write(req)
+    if err != nil {
+        client.conn.Close()
+        return nil, err
+    }
+
+    resp, err = client.conn.Read()
+    if err != nil && resp == nil {
+        client.conn.Close()
+        return nil, err
+    }
+
+    return resp, nil
+}
+
+func (client *Client) Get(url *http.URL) (result *FetchResult) {
+    //
+    req := new(http.Request)
+    req.URL = url
+    req.Header = make(map[string]string, 10)
+    req.UserAgent = "HeroshiBot/0.3 (+http://temoto.github.com/heroshi/; temotor@gmail.com)"
+
+    // debug
+    if false {
+        dump, _ := http.DumpRequest(req, true)
+        print(string(dump))
+    }
+
+    resp, err := client.Request(req)
+    if err != nil {
+        return ErrorResult(url.Raw, err.String())
+    }
+
+    b, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        return ErrorResult(url.Raw, err.String())
+    }
+
+    responseBody := string(b)
+    resp.Body.Close()
+
+    return &FetchResult{
+        Url:        url.Raw,
+        Success:    true,
+        Status:     resp.Status,
+        StatusCode: resp.StatusCode,
+        Body:       responseBody,
+        Headers:    resp.Header,
+    }
+}
---
+++ io-worker/worker.go
@@ -0,0 +1,142 @@
+package main
+
+import (
+    "fmt"
+    "http"
+    "json"
+    "os"
+    "redis"
+    "robotstxt"
+    "sync"
+    "time"
+)
+
+
+type Worker struct {
+    // When false (default), worker will obey /robots.txt
+    // when true, any URL is allowed to visit.
+    SkipRobots bool
+
+    // Keep-alive HTTP clients.
+    clients map[string]*Client
+    // clients list lock
+    cl_lk *sync.RWMutex
+
+    cache redis.Client
+}
+
+
+func newWorker() *Worker {
+    return &Worker{
+        clients: make(map[string]*Client, 1000),
+        cl_lk:   new(sync.RWMutex),
+    }
+}
+
+// Downloads url and returns whatever result was.
+// This function WILL NOT follow redirects.
+func (w *Worker) Download(url *http.URL) *FetchResult {
+    w.cl_lk.Lock()
+    client := w.clients[url.Authority]
+    if client == nil {
+        client = new(Client)
+        w.clients[url.Authority] = client
+        go func() {
+            time.Sleep(120 * 1e9)
+            w.cl_lk.Lock()
+            w.clients[url.Authority] = nil, false
+            w.cl_lk.Unlock()
+        }()
+    }
+    w.cl_lk.Unlock()
+
+    result := client.Get(url)
+
+    return result
+}
+
+func (w *Worker) CacheOrDownload(url *http.URL) *FetchResult {
+    if encoded, err := w.cache.Get(url.Raw); err == nil {
+        cached := new(FetchResult)
+        if err := json.Unmarshal(encoded, cached); err == nil {
+            return cached
+        }
+    }
+    result := w.Download(url)
+    return result
+}
+
+func (w *Worker) Fetch(url *http.URL) (result *FetchResult) {
+
+    for redirect := 0; ; redirect++ {
+        if redirect > 10 {
+            result = ErrorResult(url.Raw, "Too much redirects")
+            break
+        }
+
+        if url.Scheme == "" || url.Host == "" {
+            result = ErrorResult(url.Raw, "Incorrect URL: "+url.Raw)
+            break
+        }
+
+        // The /robots.txt is always allowed, check others.
+        if w.SkipRobots || url.Path == "/robots.txt" {
+        } else {
+            var allow bool
+            allow, result = w.AskRobots(url)
+            if !allow {
+                break
+            }
+        }
+
+        result = w.CacheOrDownload(url)
+        if ShouldRedirect(result.StatusCode) {
+            location := result.Headers["Location"]
+            var err os.Error
+            url, err = http.ParseURL(location)
+            if err != nil {
+                result = ErrorResult(url.Raw, err.String())
+                break
+            }
+            continue
+        }
+
+        // no redirects required
+        break
+    }
+    encoded, _ := json.Marshal(result)
+    w.cache.Set(url.Raw, encoded)
+    return result
+}
+
+func (w *Worker) AskRobots(url *http.URL) (bool, *FetchResult) {
+    robots_url_str := fmt.Sprintf("%s://%s/robots.txt", url.Scheme, url.Host)
+    robots_url, err := http.ParseURL(robots_url_str)
+    if err != nil {
+        return false, ErrorResult(url.Raw, err.String())
+    }
+
+    fetch_result := w.Fetch(robots_url)
+
+    if !fetch_result.Success {
+        fetch_result.Status = "Robots download error: " + fetch_result.Status
+        return false, fetch_result
+    }
+
+    var robots *robotstxt.RobotsData
+    robots, err = robotstxt.FromResponse(fetch_result.StatusCode, fetch_result.Body)
+    if err != nil {
+        fetch_result.Status = "Robots parse error: " + err.String()
+        return false, fetch_result
+    }
+
+    robots.DefaultAgent = "HeroshiBot"
+
+    var allow bool
+    allow, err = robots.Test(url.RawPath)
+    if err != nil {
+        return false, ErrorResult(url.Raw, "Robots test error: "+err.String())
+    }
+
+    return allow, nil
+}
